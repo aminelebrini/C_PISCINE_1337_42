709782f62cbfd6d024bc8f807a2ebda401cd9f1c
AMINE LEBRINI <113290995+aminelebrini@users.noreply.github.com>
2024-08-09 12:17:48 +0100

    Update ft_strlen.c

--- C_PISCINE/C01/ex06/ft_strlen.c
+++ C_PISCINE/C01/ex06/ft_strlen.c
@@ -1,13 +1,8 @@
-#include <unistd.h>
+int ft_strlen(char *str) {
+    int i = 0;
 
-void	ft_putstr(char *str)
-{
-	int	i;
-
-	i = 0;
-	while (str[i] != '\0')
-	{
-		write (1, &str[i], 1);
-		i++;
-	}
+    while (str[i] != '\0') {
+        i++;
+    }
+    return i;
 }
